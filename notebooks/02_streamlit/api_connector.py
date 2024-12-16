1962eddd1c698ed80f0e4e352d3c9b82c091ee30
Simren Basra <simrenbasra@gmail.com>
2024-12-16 16:02:01 +0000

    Refining prompts

--- notebooks/02_streamlit/api_connector.py
+++ notebooks/02_streamlit/api_connector.py
@@ -23,55 +23,59 @@ def build_prompt(char_type, genre):
     prompt = f'Write a {genre} short story about a {char_type}.'
 
     # Tailoring prompt depending on genre 
-    if genre.lower() in ('comedy', 'romcom', 'romantic comedy'):
-        prompt += 'Introduce a humourous and playful twist'
+    if genre.lower() == 'comedy':
+        prompt += 'Introduce humourous mishaps and playful twists into the story.'
+        prompt += 'The story should focus on comedic situation while keeping the the festive theme.'
     elif genre.lower() == 'action':
-        prompt += 'Story should be fast-paced with thrilling events to keep readers engaged.'
-    elif genre.lower() in ('romance', 'romcom', 'romantic comedy'):
-        prompt += 'Stroy should focus on heartwarming and emotional connections between characters.'
+        prompt += 'Story to be fast-paced with thrilling chases or rescues, set against the backdrop of Christmas.'
+    elif genre.lower() == 'romance':
+        prompt += 'Story to be centred on the emotional connection and blossoming romance between two characters.'
     elif genre.lower() == 'horror':
-        prompt += 'Introduce suspense and eerie events to spook readers but still keep to a Christmas theme.'
-    elif genre.lower() == 'msytery':
-        prompt += 'Introduce clues and unexpected turns in the plot while keeping to a Christmas theme.'
+        prompt += 'Create suspense with eerie twists and unsettling events to spook readers while keeping to a Christmas theme.'
+    elif genre.lower() == 'mystery':
+        prompt += 'Create suspense by incorporating cryptic clues and unexpected turns for a Christmas themed mystery.'
     elif genre.lower() == 'fantasy':
-        prompt += 'Set the story in imaginative placces and incoporate some festive magic.'
+        prompt += 'Set the story in imaginative places filled with whimsical creatures and magical objects.'
+    elif genre.lower() == 'sci-fi':
+        prompt += 'Blend futuristic technology and AI with a festive twist.'
+        prompt += 'To incorporate space travel or time travel in a Christmas-themed adventure.'
     else:
-        prompt += 'The one of the story should be uplifting and festive.'
+        prompt += 'The overall tone of the story should be uplifting and festive.'
 
     # To ensure the story starts off strong and engages the reader
-    prompt += 'Start with a strong, compelling hook to immediately engage the reader.'
+    prompt += "Start with a strong hook or surprising event to grab the reader's attention."
 
     # To build out the story, adding in other Christmas Characters and ensure characters develop with the story
-    prompt += 'Include other Christmas characters with distinct roles to support the main story, without overshadowing it.'
     prompt += 'Develop relateble characters who show development and growth or learn a lesson as the story develops.'
+    prompt += 'Show how Chrsitmas spirit influences the characters and drives them to succeed'
 
     # Story plot
-    prompt += 'Story must have a clear, logical flow where plots connect naturally and seamlessly.'
-    prompt += 'The story should have a realistic yet imaginative plot that shows creativity while also being believable.'
-    prompt += 'The end of the story should tie all subplots to the main plot, show resolution with a clear lesson.'
+    prompt += 'Include creative and realistic twists that make the story fun and believable.'
+    prompt += 'Make the story have a clear logical flow of events.'
 
     # Langauge 
-    #prompt += 'Use simple, clear language that is suitable for all ages. Do not use overly complex or uncommon words.'
-    prompt += 'Use simple, descriptive language to make the story and characters come alive, while avoiding overly complicated terms/words.'
-
+    prompt += 'Use simple, descriptive language to describe events, emotions and characters to make the story come alive.'
+    prompt += 'Make the story suitable for all ages by avoiding overly complex or uncommon words.'
 
     return prompt
 
 #####################################################
 # Get response from model for a given prompt
 #####################################################
-def get_response(prompt):
+def get_response(prompt, story_len):
 
     client = OpenAI()
 
     completion = client.chat.completions.create(
         model="gpt-4o",
         messages=[
-            {"role": "system", "content": "You are a creative author writing short Christmas themed stories, all stories should be 400 words. Your stories are known for their creative, unexpected plot twists"},
+            {"role": "system", "content": f"You are a creative author writing short Christmas themed stories. Ensure the story is exactly {story_len} words long, has a title and uses UK English spelling. Your stories are known for their creative, unexpected plot twists"},
             {"role": "user", "content": prompt}
-        ]
+        ],
+        max_tokens=1800 #max tokens includes in and out
 
     )
 
     return(completion.choices[0].message.content)
 
+ 
