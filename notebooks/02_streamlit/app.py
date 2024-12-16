2385aa22f3a0cc2f865d7a13820f0f3cf710a142
Simren Basra <simrenbasra@gmail.com>
2024-12-16 16:02:35 +0000

    Adding more input and some design functionalities

--- notebooks/02_streamlit/app.py
+++ notebooks/02_streamlit/app.py
@@ -2,25 +2,39 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from api_connector import get_response, build_prompt
+import time 
 
 st.title('Generate your own Christmas Story!')
 
-st.header('Inputs')
+st.subheader('Inputs')
 
-character_type = st.text_input(label = 'Character Type', placeholder = 'Elf')
+character_type = st.text_input(label = 'Character Type', placeholder = 'Like Elf, Santa, Reindeer, Snowman, etc.', )
 
-story_genre = st.text_input(label = 'Genre', placeholder = 'Comedy')
+story_genre = st.selectbox( 'Genre',
+                           ('Comedy', 'Action', 'Fantasy', 'Horror', 'Romance', 'Sci-Fi', 'Mystery'),
+                           placeholder = 'Select a Genre', 
+                           index = None # to get placeholder text to show
+                           )
 
+story_length = st.slider('Story Length ', min_value = 300, max_value = 1000, step = 100, format='%d words', value = 500)
 
 if st.button(label = 'Generate Story'):
 
     st.subheader('Story:')
 
-    my_prompt = build_prompt(character_type,story_genre)
+    if not character_type:
+        st.error('Please enter a character type')
+    elif not story_genre:
+        st.error('Please enter a genre')
+    else:
+        with st.spinner('Generating, please wait...'):
+            st.snow()
 
-    my_story = get_response(my_prompt)
+            my_prompt = build_prompt(character_type,story_genre)
 
-    st.write(my_story)
+            my_story = get_response(my_prompt,story_length)
 
-    # Button for user to download story
-    st.download_button("Download your story!", my_story)
+            st.write(my_story)
+
+            # Button for user to download story
+            st.download_button('Download your story 📥', my_story, file_name = 'my_generated_story.txt')
